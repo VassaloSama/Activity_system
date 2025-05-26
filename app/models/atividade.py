0e5ebca70a833070b9984099d4c1b679f6a540d2
Lucas Vassalo <138052789+VassaloSama@users.noreply.github.com>
2025-05-26 16:23:37 -0300

    feat: model de atividade

---
+++ app/models/atividade.py
@@ -0,0 +1,15 @@
+from app.config.database import db
+
+class Atividade(db.Model):
+    __tablename__ = 'atividades'
+
+    atividade_id = db.Column(db.Integer, primary_key=True)
+    professor_id = db.Column(db.Integer, nullable=False)
+    enunciado = db.Column(db.String(100), nullable=False)
+
+    def serialize(self):
+        return {
+            'atividade_id': self.atividade_id,
+            'professor_id': self.professor_id,
+            'enunciado': self.enunciado
+        }
